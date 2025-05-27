005f65f20bcc939df610f075afe8b1ba9bb5f2c8
abdulaziz27 <doelziez212@gmail.com>
2025-05-27 11:09:21 +0700

    build for production

--- public/build/assets/Edit-Bt6_h_aU.js
+++
@@ -1,1 +0,0 @@
-import{r,S as i}from"./app-Cnd1CekA.js";/* empty css            */function e({order:t}){return r.useEffect(()=>{i.visit(`/admin/orders/${t.id}`)},[t.id]),null}export{e as default};
--- public/build/assets/input-error-CVIexekj.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-Cnd1CekA.js";import{a as o}from"./index-Br2mh7EY.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
